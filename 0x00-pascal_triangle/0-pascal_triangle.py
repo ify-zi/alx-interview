5b719d48a1050c9a81a210c287be417c2527fa81
Ifeanyi Zion Chidiebere <ifeanyizion3@gmail.com>
2024-06-27 20:51:20 +0000

    pascal triangle

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+'''pascal triangle'''
+
+
+def pascal_triangle(n):
+    '''
+    Args: n (int): The number of rows of the triangle
+    Returns: List of lists of integers representing the Pascal’s triangle
+    '''
+    lists = []
+    if n == 0:
+        return lists
+    for i in range(n):
+        lists.append([])
+        lists[i].append(1)
+        if (i > 0):
+            for j in range(1, i):
+                lists[i].append(lists[i - 1][j - 1] + lists[i - 1][j])
+            lists[i].append(1)
+    return lists
