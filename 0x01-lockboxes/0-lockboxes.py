3789caf0a7074a20482be0b584852cc991f1de5f
Ifeanyi Zion Chidiebere <ifeanyizion3@gmail.com>
2024-07-03 13:39:12 -0700

    locbox algorithm

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -2,11 +2,12 @@
 """ function that check if a key can unlock all box"""
 
 def canUnlockAll(boxes):
+    """ lockbox algorithm """
     length = len(boxes)
     i = 0
     opened = []
     keyVal = len(boxes[0])
-    while i < length:
+    while i <= length:
         for box in boxes:
             key = len(box)
             if key <= keyVal:
