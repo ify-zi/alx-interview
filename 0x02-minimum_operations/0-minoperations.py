6feeda076ddbcaccd95aca478c1cb0f07cfae27e
Ifeanyi Zion Chidiebere <ifeanyizion3@gmail.com>
2024-07-10 14:47:54 -0700

    markdown file

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""Module for Minimum operations algorithm"""
+
+
+def minOperations(n):
+    """
+    Calculates fewest number of operations
+    """
+
+    if n is None or n < 1:
+        return 0
+
+
+    operations = 0
+    factors = 2
+
+
+    while factors <= n:
+        while n % factors == 0:
+            operations += factors
+            n //= factors
+        factors += 1
+
+    return operations
